77d89107063f76ed5b5964e35ef7bf4e597a23f3
Joshua Philips <jmichaelphilips@gmail.com>
2017-11-19 09:08:49 -0600

    Completed the next exercist.

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,7 +1,9 @@
 extern crate chrono;
+extern crate time;
 use chrono::*;
+use time::Duration;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    unimplemented!()
+    start + Duration::seconds(1000000000)
 }
